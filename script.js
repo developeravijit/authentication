d8ec76f51344d3a036ee4e4e61dd1b987997965d
developeravijit <roysidharth1996@gmail.com>
2026-02-25 18:50:27 +0530

    update

--- script.js
+++ script.js
@@ -11,11 +11,27 @@ overlayBtn.addEventListener("click", () => {
   });
 });
 
-let register = (event) => {
+let register = async (event) => {
   event.preventDefault();
   const form = event.target;
   const fullName = form.fullName.value.trim();
   // console.log(fullName);
   const email = form.email.value.trim();
   const password = form.password.value.trim();
+
+  const nameError = document.getElementById("nameError");
+  const emailError = document.getElementById("emailError");
+  const passError = document.getElementById("passError");
+
+  nameError.textContent = "";
+  emailError.textContent = "";
+  passError.textContent = "";
+
+  if (fullName.length <= 3) {
+    nameError.textContent = "Name is not valid";
+  }
+
+  if (!email.includes("@") && !email.endsWith(".com")) {
+    emailError.textContent = "Email is not valid";
+  }
 };
