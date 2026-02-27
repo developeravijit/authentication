9cfe0e39b0d80429a5dbc6dc6fd76546e09f1b59
developeravijit <roysidharth1996@gmail.com>
2026-02-28 01:06:11 +0530

    bug fix

--- logout.js
+++ logout.js
@@ -9,13 +9,13 @@ const userName = document.getElementById("userName");
 // console.log("token", token);
 userName.textContent = user;
 
-if (!token || !user) {
+if (!token && !user) {
   window.location.href = "index.html";
 }
 
 let logout = () => {
   localStorage.removeItem("token");
-  localStorage.removeItem("user");
+  localStorage.removeItem("userName");
   setTimeout(() => {
     window.location.href = "index.html";
   }, 1000);
